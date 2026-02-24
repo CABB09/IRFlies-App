d44e1bad4e61c6b78db2bfa2c26f3f092bec1db8
Carlos Alexis Barrios Bello <144065079+CABB09@users.noreply.github.com>
2026-02-24 12:58:32 -0600

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-# Hace que "from core import ..." funcione dentro de tests
-import sys, os
-APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # .../app
-if APP_ROOT not in sys.path:
-    sys.path.insert(0, APP_ROOT)
